81591f3f461cd38a15bd7e0daff8be79e4121fae
zanebennion <zanebennion@zben.me>
2026-07-06 22:09:38 -0800

    added cursor file but not working yet

--- hyprland/.config/hypr/config/environment.lua
+++ hyprland/.config/hypr/config/environment.lua
@@ -1,2 +1,5 @@
 -- Environmental variables
 -- if you don't use UWSM, define your variables here (e.g. hl.env("QT_QPA_PLATFORM", "wayland"))
+
+hl.env("HYPRCURSOR_THEME", "Hackneyed")
+hl.env("HYPRCURSOR_SIZE", "24")
